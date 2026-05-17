6553e97c3f780d6fc13a8bd7d4ac5a5aa81fea42
IsarapuSatyasai <esarapusathyasai603@gmail.com>
2026-05-17 09:42:48 +0530

    feat: removed the bug modulus

--- src/core_ops.py
+++ src/core_ops.py
@@ -8,9 +8,3 @@ class CoreCalculator:
     def subtract(self, a: float, b: float) -> float:
         """Returns the difference of a and b."""
         return a - b
-    
-    def modulus(self, a: float, b: float) -> float:
-        """Calculates the modulus of a by b."""
-        if b == 0:
-            raise ValueError("Cannot perform modulus by zero.")
-        return a % b
